e672449bfdfb871e1622c93a9e8e1c14ae0e1fa0
plxgwalker <olegsuv.ukr@gmail.com>
2024-04-18 12:33:47 +0300

    mod: better user prediction

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import numpy as np
 import data_pre_processing as dpp
 import model as m
 
+from sklearn.discriminant_analysis import StandardScaler
+
 
 if __name__ == '__main__':
     user_input = {
@@ -12,11 +14,13 @@ if __name__ == '__main__':
         'Low': 22306.8,
         'Volume': 240218
     }
-    # real close price: $22,609.16
     ordinal_date = dt.datetime.strptime(
         user_input['Date'], '%Y-%m-%d').date().toordinal()
     user_input['Date'] = ordinal_date
     user_input = np.array(list(user_input.values())).reshape(1, -1)
+
+    scaler = StandardScaler()
+    user_input = scaler.fit_transform(user_input)
 
     dpp = dpp.DataPreProcessing()
     dpp.load_data('data')
@@ -26,6 +30,7 @@ if __name__ == '__main__':
     model = m.StockPredictor()
     model.train(dpp.x_train, dpp.y_train)
     y_pred = model.predict(user_input)
-    print(y_pred)
+    print(y_pred)  # real close price: $22,609.16
+
     # model.benchmark(dpp.y_train, y_pred)
     # model.plot(dpp.x_train, dpp.y_train, y_pred)
